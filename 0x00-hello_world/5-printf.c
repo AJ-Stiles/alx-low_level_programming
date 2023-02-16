5e7eb4b2a1f8c0ab42fc7da807b7cce7eb9c5ada
Alistair <jude55593@gmail.com>
2023-02-16 03:44:44 -0800

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * use the function printf
+ * you are not allowed to use the function puts
+ * your program should return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
