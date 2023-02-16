36854ca7187e3db86d76546435ee122ef3c99d55
Alistair <jude55593@gmail.com>
2023-02-16 03:47:01 -0800

    using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
- * use the function printf
- * you are not allowed to use the function puts
- * your program should return 0
+ * main -Entry
+ * Return: Always 0
  */
 int main(void)
 {
