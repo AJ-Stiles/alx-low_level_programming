8ee32b64261e48c86253899e490b5e621cb638b0
Alistair <jude55593@gmail.com>
2023-02-17 06:04:31 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *  * main - Entry
+ *   * Return: Always 0
+ */
+{
+		char ch;
+
+			for (ch = 'z'; ch >= 'a'; ch--)
+					{
+								putchar(ch);
+									}
+				putchar('\n');
+					return (0);
+}
+
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *  * main - Entry
+ *   * Return: Always 0
+ */
+int main(void)
+{
+		int n;
+			char ch;
+
+				for (n = 48; n < 58; n++)
+						{
+									putchar(n);
+										}
+					for (ch = 'a'; ch <= 'f'; ch++)
+							{
+										putchar(ch);
+											}
+						putchar('\n');
+							return (0);
+}
+
