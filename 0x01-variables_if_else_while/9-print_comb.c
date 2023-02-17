8c2375ef6e3cc8c64aa7508200743d843ce8bd85
Alistair <jude55593@gmail.com>
2023-02-17 08:28:24 -0800

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,23 +3,22 @@
 #include <time.h>
 
 /**
- *  * main - Entry
- *   * Return: Always 0
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
 {
-		int n;
+	int n;
 
-			for (n = 48; n < 58; n++)
-					{
-								putchar(n);
-										if (n != 57)
-													{
-																	putchar(',');
-																				putchar(' ');
-																						}
-											}
-				putchar('\n');
-					return (0);
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
-
