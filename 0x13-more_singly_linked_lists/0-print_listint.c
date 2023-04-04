aec70287306e629650f6045d72bfc7c4457056d6
Jude Alistair <jude55593@gmail.com>
2023-04-04 04:06:19 -0700

    Print lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,10 +9,33 @@
 size_t print_listint(const listint_t *head)
 {
     size_t num_nodes = 0;
+    char num_buffer[12];  // Buffer to store integer as a string
 
     while (head != NULL)
     {
-        printf("%d\n", head->n);
+        // Convert integer to a string
+        int num_digits = snprintf(num_buffer, sizeof(num_buffer), "%d", head->n);
+
+        // Allocate memory for the string on the heap
+        char *str = malloc((num_digits + 1) * sizeof(char));
+        if (str == NULL)
+        {
+            exit(EXIT_FAILURE);
+        }
+
+        // Copy the string from the buffer to the heap
+        for (int i = 0; i <= num_digits; i++)
+        {
+            *(str + i) = *(num_buffer + i);
+        }
+
+        // Print the string
+        write(1, str, num_digits);
+        write(1, "\n", 1);
+
+        // Free the allocated memory
+        free(str);
+
         num_nodes++;
         head = head->next;
     }
